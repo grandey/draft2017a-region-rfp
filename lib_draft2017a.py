e020b9ace3397ccf2aa4b4ef065e75529075a326
Benjamin Grandey <benjamin.grandey@gmail.com>
2018-03-27 16:47:35 +0800

    Added load_zonal_stats() to lib_draft2017a.py

--- lib_draft2017a.py
+++ lib_draft2017a.py
@@ -340,22 +340,22 @@ def load_regional_stats(scenario_combination='All1-All0',
             mean: area-weighted regional mean annual mean (averaged across different years)
             error: combined standard error
             ci99: 99% confidence interval (based on 2.576*error)
-            p_value: p-value for difference between scenarios (NaN if not available)
+            p_value: p-value for difference between scenarios
             contributing_scenarios: list of contributing scenarios (e.g. ['All1', 'All0'])
     """
     # Check if regional stats have been calculated previously
     try:
         result = _regional_stats_dict[(scenario_combination, variable, region)]
     except KeyError:
-        # Initialise dictionary with input arguments and np.nan/None
+        # Initialise dictionary with input arguments and None
         result = {'scenario_combination': scenario_combination,
                   'variable': variable,
                   'region': region,
                   'awms': None,
-                  'mean': np.nan,
-                  'error': np.nan,
+                  'mean': None,
+                  'error': None,
                   'ci99': None,
-                  'p_value': np.nan,
+                  'p_value': None,
                   'contributing_scenarios': None}
         # Load region bounds
         lon_bounds, lat_bounds = _region_bounds_dict[region]
@@ -440,6 +440,124 @@ def load_regional_stats(scenario_combination='All1-All0',
         result['ci99'] = ci99
         # Save result for future reference
         _regional_stats_dict[(scenario_combination, variable, region)] = result
+    # Return result
+    return result
+
+
+_zonal_stats_dict = {}  # dictionary to hold results from load_zonal_stats()
+
+
+def load_zonal_stats(scenario_combination='All1-All0',
+                     variable='SWCF_d1'):
+    """
+    Load dictionary of zonal mean statistics for a specific variable and scenario.
+
+    Args:
+        scenario_combination: scenario combination (default 'All1-All0')
+        variable: name of variable (default 'SWCF_d1')
+
+    Returns:
+        dictionary, with the following keys:
+            scenario_combination: as per input arg
+            variable: as per input arg
+            zonmeans: DataArray of zonal-mean annual-means for different years (if single scenario)
+            mean: DataArray of zonal-mean annual-mean (averaged across different years)
+            error: DataArray of combined standard errors
+            ci99: DataArray of 99% confidence intervals (based on 2.576*error)
+            p_value: array of p-values for difference between scenarios
+            contributing_scenarios: list of contributing scenarios (e.g. ['All1', 'All0'])
+    """
+    # Check if zonal stats have been calculated previously
+    try:
+        result = _zonal_stats_dict[(scenario_combination, variable)]
+    except KeyError:
+        # Initialise dictionary with input arguments and None
+        result = {'scenario_combination': scenario_combination,
+                  'variable': variable,
+                  'zonmeans': None,
+                  'mean': None,
+                  'error': None,
+                  'ci99': None,
+                  'p_value': None,
+                  'contributing_scenarios': None}
+        # Case 1: scenario_combination is a single scenario
+        if scenario_combination in _inverted_scenario_name_dict:
+            result['contributing_scenarios'] = [scenario_combination, ]
+            # Load annual data
+            data = load_output(variable,
+                               scenario=_inverted_scenario_name_dict[scenario_combination],
+                               season='annual', apply_sf=True)
+            # Calculate zonal means for different years
+            zonmeans = data.mean(dim='lon')
+            result['zonmeans'] = zonmeans
+            # Mean and standard error across years
+            n_years = zonmeans['year'].size
+            mean = zonmeans.mean(dim='year')  # mean across years
+            error = zonmeans.std(dim='year', ddof=1) / np.sqrt(n_years)  # standard error
+            result['mean'] = mean
+            result['error'] = error
+        # Case 2: scenario_combination is a difference between two scenarios
+        elif scenario_combination.split('-')[0] in _inverted_scenario_name_dict:
+            scenario1, scenario2 = scenario_combination.split('-')
+            result['contributing_scenarios'] = [scenario1, scenario2]
+            # Call recursively to get zonal stats for each scenario
+            stats1 = load_zonal_stats(scenario_combination=scenario1, variable=variable)
+            stats2 = load_zonal_stats(scenario_combination=scenario2, variable=variable)
+            # Combine to get difference between means and the combined error
+            mean = stats1['mean'] - stats2['mean']
+            error = np.sqrt(stats1['error']**2 + stats2['error']**2)
+            result['mean'] = mean
+            result['error'] = error
+            # p-value based on standard two-sample t-test
+            p_value = ttest_ind(stats1['zonmeans'], stats2['zonmeans'], equal_var=True)[1]
+            result['p_value'] = p_value
+        # Case 3: scenario_combination is ∑(Θ1-All0)
+        elif scenario_combination == '$\Sigma_{\Theta}$($\Theta$1-All0)':
+            theta1_scenarios = [s for s in _inverted_scenario_name_dict.keys() if
+                                (s[-1] == '1' and s not in ['Correct1', 'All1'])]
+            if len(theta1_scenarios) != 10:
+                raise RuntimeError('theta1_scenarios = {}'.format(theta1_scenarios))
+            result['contributing_scenarios'] = theta1_scenarios + ['All0', ]
+            # Call recursively to get lists of means and errors for each Θ1-All0 combination
+            mean_list = []
+            error_list = []
+            for scenario in theta1_scenarios:
+                temp_stats = load_zonal_stats(scenario_combination='{}-All0'.format(scenario),
+                                              variable=variable)
+                mean_list.append(temp_stats['mean'])
+                error_list.append(temp_stats['error'])
+            # Combine to get sum of means and the combined error
+            mean = sum(mean_list)
+            error = np.sqrt(sum([e**2 for e in error_list]))
+            result['mean'] = mean
+            result['error'] = error
+        # Case 4: scenario_combination is ∑(All1-Θ0)
+        elif scenario_combination == '$\Sigma_{\Theta}$(All1-$\Theta$0)':
+            theta0_scenarios = [s for s in _inverted_scenario_name_dict.keys() if
+                                (s[-1] == '0' and s != 'All0')]
+            if len(theta0_scenarios) != 10:
+                raise RuntimeError('theta0_scenarios = {}'.format(theta0_scenarios))
+            result['contributing_scenarios'] = theta0_scenarios + ['All1', ]
+            # Call recursively to get lists of means and errors for each All1-Θ0 combination
+            mean_list = []
+            error_list = []
+            for scenario in theta0_scenarios:
+                temp_stats = load_zonal_stats(scenario_combination='All1-{}'.format(scenario),
+                                              variable=variable,)
+                mean_list.append(temp_stats['mean'])
+                error_list.append(temp_stats['error'])
+            # Combine to get sum of means and the combined error
+            mean = sum(mean_list)
+            error = np.sqrt(sum([e ** 2 for e in error_list]))
+            result['mean'] = mean
+            result['error'] = error
+        else:
+            raise ValueError('scenario_combination not recognized')
+        # 99% confidence interval based on standard error
+        ci99 = (mean - 2.576 * error, mean + 2.576 * error)
+        result['ci99'] = ci99
+        # Save result for future reference
+        _zonal_stats_dict[(scenario_combination, variable)] = result
     # Return result
     return result
 
